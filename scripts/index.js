54f225f64896faa7a8f767de3fc910f8f0e07d71
Nikolai Musatov <musatovnikolajj@rambler.ru>
2023-11-09 09:56:16 +0400

    Version 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesContainer = document.querySelector(".places__list");
+const placeTemplate = document.querySelector("#card-template").content;
+
+function createCard(i, func) {
+  const placeElement = placeTemplate
+  .querySelector(".places__item")
+  .cloneNode(true);
+  placeElement.querySelector(".card__title").textContent = i.name;
+  placeElement.querySelector(".card__image").src = i.link;
+  placeElement.querySelector(".card__delete-button").addEventListener('click', func);
+  return placeElement;
+};
+
+function deleteCard(evt) {
+  const listItem = evt.target.closest(".places__item");
+  listItem.remove();
+};
+
+initialCards.forEach((item) => {
+  const newCard = createCard(item, deleteCard);
+  placesContainer.append(newCard);
+});
+
+
